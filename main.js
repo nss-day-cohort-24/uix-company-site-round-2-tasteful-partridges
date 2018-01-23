c63d588764cbdd0c3113f1e75ccd172ff5298f3c
Dan Watson <danwatson00@yahoo.com>
2018-01-23 15:49:41 -0600

    ppplleeease

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var mobileapp = {
     name: "Mobile App - Time Management",
     description: "Time management to organize your schedule",
     price: "$20.00",
-    URL: "image.jpg" //insert image
+    URL: "/img/proddetail.jpg"
 }
 
-cartCard = `<div id="cart-container"><img class="cartCard_img" src="${mobileapp.URL}"><br><p>${mobileapp.name}<br>${mobileapp.description}<br>${mobileapp.price}</p></div>`;
+cartCard = `<div id="cart-container"><img class="cartCardimg" src="${mobileapp.URL}"><br><p>${mobileapp.name}<br>${mobileapp.description}<br>${mobileapp.price}</p></div>`;
 
 document.getElementById("cart-container").innerHTML = cartCard;
