eb492d29d6acaedcf0eca7189a91fa738e389a88
Ben Atkins <benjatkins13@gmail.com>
2018-01-23 11:35:00 -0600

    fixed js

--- form.js
+++ form.js
@@ -2,17 +2,16 @@ function validateForm() {
     var x = document.forms["myForm"]["firstname"].value;
     var y = document.forms["myForm"]["lastname"].value;
     var z = document.forms["myForm"]["email"].value;
-    var c = document.forms["myForm"]["comment"].value
     if (x == "") {
         alert("First Name must be filled out");
         return false;
     }
     else if (y == "") {
         alert("Last Name must be filled out");
+        return false
     }
     else if (z == ""){
         alert("Email must be filled out");
-
+        return false
+    }
 }
-
-
