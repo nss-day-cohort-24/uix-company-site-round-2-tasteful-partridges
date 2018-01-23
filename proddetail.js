a48ead3c8c9ac91f077cbc903db63ee31ba1f292
Ben Atkins <benjatkins13@gmail.com>
2018-01-23 15:54:02 -0600

    nothing

--- proddetail.js
+++ proddetail.js
@@ -1,8 +1,8 @@
 var reviews = [
-    {review: "Tidy Task changed my life. I now have time to play video games, spend time with my girlfriend, eat, get all my assignments done, and go to the gym,", author: "Ben Atkins"},
+    {review: "Tidy Task changed my life. I now have time to play video games, spend time with my girlfriend,eat, get all my assignments done, and go to the gym,", author: "Ben Atkins"},
  ];
 
- var reviewHtml = " ";
+ var reviewHtml = "";
 for (i = 0; i < reviews.length; i++) {
 
    reviewHTML = `<p>`;
