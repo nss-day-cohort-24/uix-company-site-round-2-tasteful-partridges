712b6a490b1dcc8f044b303558e7c40129a9ecdd
Ben Atkins <benjatkins13@gmail.com>
2018-01-24 09:20:55 -0600

    nothing

--- proddetail.js
+++ proddetail.js
@@ -1,14 +1,15 @@
 var reviews = [
     {review: "Tidy Task changed my life. I now have time to play video games, spend time with my girlfriend,eat, get all my assignments done, and go to the gym,", author: "Ben Atkins"},
+    {review: "This is my 2nd Review", author: "Ben Atkins"}
  ];
 
- var reviewHtml = "";
-for (i = 0; i < reviews.length; i++) {
+ revHtml = "";
+for (i = 0; i = 2; i++) {
 
-   reviewHTML = `<p>`;
-   reviewHTML += `${reviews[i].review}`;
-   reviewHTML += " - "
-   reviewHTML += `${reviews[i].author}`;
-   reviewHTML += `</p>`;
+   revHTML += `<p>`;
+   revHTML += `${reviews[i].review}`;
+   revHTML += ' - ';
+   revHTML += `${reviews[i].author}`;
+   revHTML += `</p>`;
 }
 document.getElementById("critic").innerHTML = reviewHTML;
