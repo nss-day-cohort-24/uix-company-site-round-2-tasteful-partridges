e5c2b9f8c393b53ed172f67231b83175b2a8d765
Ben Atkins <benjatkins13@gmail.com>
2018-01-24 09:44:30 -0600

    PDJS

--- proddetail.js
+++ proddetail.js
@@ -1,15 +1,28 @@
 var reviews = [
-    {review: "Tidy Task changed my life. I now have time to play video games, spend time with my girlfriend,eat, get all my assignments done, and go to the gym,", author: "Ben Atkins"},
-    {review: "This is my 2nd Review", author: "Ben Atkins"}
+    {review: "Tidy Task changed my life. I now have time to play video games, spend time with my girlfriend,eat, get all my assignments done, and go to the gym,", author: "Trevor Noah"},
+    {review: "This is my Review; I'm still hopelessly addicted to this product", author: "Stephon Diggs"}
  ];
 
- revHtml = "";
-for (i = 0; i = 2; i++) {
+revHtml = "";
+revTwo = ""
 
-   revHTML += `<p>`;
-   revHTML += `${reviews[i].review}`;
-   revHTML += ' - ';
-   revHTML += `${reviews[i].author}`;
-   revHTML += `</p>`;
-}
-document.getElementById("critic").innerHTML = reviewHTML;
+revHTML = `<p>`;
+revHTML += `${reviews[0].review}`;
+revHTML += ' - ';
+revHTML += `${reviews[0].author}`;
+revHTML += `</p>`;
+
+revTwo = `<p>`;
+revTwo += `${reviews[1].review}`;
+revTwo += ' - ';
+revTwo += `${reviews[1].author}`;
+revTwo += `</p>`;
+
+document.getElementById("critic").innerHTML = revHTML;
+document.getElementById("critic2").innerHTML = revTwo;
+
+
+
+
+
+
